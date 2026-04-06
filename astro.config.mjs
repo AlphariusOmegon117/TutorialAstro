dc9140986e08f0310260e23b56b1b81e5c981de4
AlphariusOmegon117 <a01278947@tec.mx>
2026-04-05 21:06:49 -0600

    Pasos finales y extras en blog, movimiento de los post a otra carpeta,grettigs, etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [react(), preact()]
+});
